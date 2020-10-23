b7a55e0a5de320093d5a1931199ad2fe6d599c38
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-23 16:42:31 -0700

    orm adjustments complete, made them based on MVC activity 16

--- config/orm.js
+++ config/orm.js
@@ -1,44 +1,66 @@
 const connection = require("./connection.js");
 
+
+//Array function passes through ? value where needed for SQL to work: ref MVC, act 16
 function addQuestionMark(num) {
     let array = [];
-    for (let i = 0; i < num; i++){
+    for (let i = 0; i < num; i++) {
         array.push("?");
     };
     return array.toString();
 }
 
+// loop through the keys and push the key/value as a string int arr: ref MVC, act 16
+function objToSql(ob) {
+    let arr = [];
+    for (const key in ob) {
+        const value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
+//orm drawn from MVC - activity 16
 const orm = {
-    selectAll: function(tableValue, cb){
-        const queryString = "SELECT * FROM ??"
-        connection.query(queryString, [tableValue], (err, result) => {
+    selectAll: function (tableInput, cb) {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
             if (err) throw err;
             console.log(result);
             cb(result);
         });
     },
 
-    insertOne: function(table, tableCol, tableValue, cb){
+    insertOne: function (table, cols, vals, cb) {
         const queryString = "INSERT INTO burgers" + table;
-       //activity 16 uses this method to complete the string
+        //activity 16 uses this method to complete the string
         queryString += " (";
-        queryString += tableCol.toString();
+        queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
-        queryString += addQuestionMark(tableValue.length);
+        queryString += addQuestionMark(vals.length);
         queryString += ")";
 
         console.log(queryString);
-        connection.query(queryString, [tableInput], (err, result) => {
+        connection.query(queryString, vals, (err, result) => {
             if (err) throw err;
             console.log(result);
             cb(result);
         });
     },
 
-    updateOne: function(tableValue, tableCol, condition, cb){
-        const queryString = "UPDATE " + tableValue;
-        
+    updateOne: function (table, objColVals, condition, cb) {
+        const queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
         console.log(queryString);
         connection.query(queryString, (err, result) => {
             if (err) {
