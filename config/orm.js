9b4ac76aff34777c8f3b90361a297ef83bfe846a
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-23 16:10:46 -0700

    reviewed activities, take new direction for code

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,42 @@
 const connection = require("./connection.js");
 
 const orm = {
-    selectAll: function(){},
+    selectAll: function(tableValue, cb){
+        const queryString = "SELECT * FROM ??"
+        connection.query(queryString, [tableValue], (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        });
+    },
 
-    insertOne: function(){},
+    insertOne: function(tableInput, cb){
+        const queryString = "INSERT INTO burgers (??) VALUES (?)"
 
-    updateOne: function(){},
+        console.log(queryString);
+        queryString += " SET ";
+        queryString += objToSql(tableCol);
+        queryString += " WHERE ";
+        queryString += condition;
+        connection.query(queryString, [tableInput], (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        })
+    },
+
+    updateOne: function(tableValue, tableCol, condition, cb){
+        const queryString = "UPDATE " + tableValue;
+        
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        })
+    },
 };
 
 module.exports = orm;
