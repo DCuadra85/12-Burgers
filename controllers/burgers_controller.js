99af50fbf60447d0f261fff3c5979e58e6fe20a0
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-23 17:09:39 -0700

    post code added to controller, need put/patch code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,28 @@
 const express = require("express")
+const router = expess.Router();
+
+const burger = require("../models/burger");
+
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        const hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burger", (req, res) => {
+    console.log(req.body.name);
+    burger.insertOne(["burgerName", "devour"], [req.body.burgerName, req.body.devour],
+    (result) => {
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burger/:id", (req, res) => {
+
+});
+
+module.exports = router;
