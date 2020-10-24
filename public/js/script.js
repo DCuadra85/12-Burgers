e8567e85cc0692e6af76d60d262e1386cb989dc0
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-10-23 20:43:54 -0700

    value adjustmetns

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,7 @@ $(function () {
         const id = $(this).data("id");
         console.log("ID:" + id);
 
-        const devoured = {
+        const devour = {
             devour: true
         };
 
@@ -21,12 +21,12 @@ $(function () {
 });
 
 
-$(".addBurger").on("click", function(event) {
+$(".addburger").on("click", function(event) {
     event.preventDefault();
 
-    const addBurger = {
+    var addBurger = {
         burgerName: $("#burgerArea").val(),
-        devoured: 0
+        devour: 0
     };
 
     $.ajax("/api/burger", {
@@ -38,47 +38,3 @@ $(".addBurger").on("click", function(event) {
         location.reload();
     });
 });
-// $(function() {
-//     $(".change-sleep").on("click", function(event) {
-//       var id = $(this).data("id");
-//       var newSleep = $(this).data("newsleep");
-
-//       var newSleepState = {
-//         sleepy: newSleep
-//       };
-
-//       // Send the PUT request.
-//       $.ajax("/api/cats/" + id, {
-//         type: "PUT",
-//         data: newSleepState
-//       }).then(
-//         function() {
-//           console.log("changed sleep to", newSleep);
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-
-//     $(".create-form").on("submit", function(event) {
-//       // Make sure to preventDefault on a submit event.
-//       event.preventDefault();
-
-//       var newCat = {
-//         name: $("#ca").val().trim(),
-//         sleepy: $("[name=sleepy]:checked").val().trim()
-//       };
-
-//       // Send the POST request.
-//       $.ajax("/api/cats", {
-//         type: "POST",
-//         data: newCat
-//       }).then(
-//         function() {
-//           console.log("created new cat");
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-//   });
